2b2cf946b186dda2cfdb119df2e8b035b9f3bfff
Cristian Branet <cristix231@gmail.com>
2025-07-13 10:07:28 +0000

    Add LiteLLM

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os, sys
 import boto3
-from dotenv import load_dotenv
 from agents import Runner, Agent
 from agent.ec2_tools import create_ec2_instance, stop_ec2_instance, start_ec2_instance, list_ec2_instances, list_ec2_instance
+from agents.extensions.models.litellm_model import LitellmModel
 
-load_dotenv()
+model = os.getenv("MODEL")
+api_key = os.getenv("LLM_API_KEY")
 
 if __name__ == "__main__":
     
@@ -17,6 +18,7 @@ if __name__ == "__main__":
     agent = Agent(
         name="Assistant",
         instructions="You are a helpful assistant for AWS EC2 management.",
+        model=LitellmModel(model=model, api_key=api_key),
         tools=[create_ec2_instance, stop_ec2_instance, start_ec2_instance, list_ec2_instances, list_ec2_instance]
     )
 
