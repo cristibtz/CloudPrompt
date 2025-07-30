5e6e7d3370e6f6c501112a00d2af79dfb19925e8
Cristian Branet <cristix231@gmail.com>
2025-07-30 15:30:21 +0000

    Added first frontend version

---
+++ cloudprompt-frontend/src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Toaster } from 'sonner'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+    <Toaster 
+      position="top-right"
+      richColors
+      expand={true}
+      duration={4000}
+    />
+  </StrictMode>,
+)
