c44c97ecd6565755225ab6ebfab8cd21a07831cb
cristibtz <cristix231@gmail.com>
2025-08-08 14:05:58 +0300

    Added new gpt 5 model

--- cloudprompt/agent/costs.py
+++ cloudprompt/agent/costs.py
@@ -9,6 +9,7 @@ MODEL = os.getenv("MODEL")
 
 # Define Model costs per 1000 tokens
 MODEL_COSTS = {
+    "gpt-5-mini" : {"input": 0.000250, "output": 0.002},
     "gpt-4o-mini": {"input": 0.000150, "output": 0.000600}, # Best
     "gpt-4.1-nano":{ "input": 0.00010, "output": 0.000400},
     "o3-mini": {"input": 0.0011, "output": 0.0044},
