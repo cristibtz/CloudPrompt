7d741a85dc006bd8582b0e9e6fe694f0c2134ca5
Cristian Branet <cristix231@gmail.com>
2025-08-12 12:22:42 +0000

    Added proxmox agent

--- mcp_server/tools/proxmox_tools/proxmox_tools.py
+++ mcp_server/tools/proxmox_tools/proxmox_tools.py
@@ -12,22 +12,41 @@ proxmox = ProxmoxAPI(
 
 logger = logging.getLogger("proxmox_tools_mcp")
 
-
 def register_tools(mcp):
     
     @mcp.tool()
-    async def list_vms():
-        try:
-            vms = []
-            for node in proxmox.nodes.get():
-                for vm in proxmox.nodes(node["node"]).qemu.get():
+    async def list_vms(
+        node_name: str 
+    ):
+        '''
+        List all VMs on a Proxmox node or all nodes if no node is specified.
+        :param node_name: Name of the Proxmox node to list VMs from. If not provided, lists VMs from all nodes.
+        :return: Dict[str, str] - List of VMs with their VMID, name, and status.
+        '''
+        if not node_name:
+            try:
+                vms = []
+                for node in proxmox.nodes.get():
+                    for vm in proxmox.nodes(node["node"]).qemu.get():
+                        vms.append({
+                            "vmid": vm["vmid"],
+                            "name": vm["name"],
+                            "status": vm["status"]
+                        })
+                return vms
+            except Exception as e:
+                logger.error(f"Error listing VMs: {e}")
+                return {"error": str(e)}
+        else:
+            try:
+                vms = [{"node_name": f"{node_name}"}]
+                for vm in proxmox.nodes(node_name).qemu.get():
                     vms.append({
                         "vmid": vm["vmid"],
                         "name": vm["name"],
                         "status": vm["status"]
                     })
-            return vms
-        except Exception as e:
-            logger.error(f"Error listing VMs: {e}")
-            return {"error": str(e)}
-        
+                return vms
+            except Exception as e:
+                logger.error(f"Error listing VMs for node {node_name}: {e}")
+                return {"error": str(e)}
