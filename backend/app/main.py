6fb0927e5924c3196f989928dedc68e7e75671a3
Cristian Branet <cristix231@gmail.com>
2025-08-26 12:40:12 +0000

    Add users to db in certain events

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,8 @@
 import asyncio
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.v1 import execute  # Import your router modules
+from app.api.v1 import execute
+from app.api.v1 import users
 import json, ast
 from app.auth import auth
 
@@ -21,7 +22,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# app.include_router(users.router, prefix="/api/v1", tags=["users"])
+app.include_router(users.router, prefix="/api/v1/users", tags=["users"])
 # app.include_router(prompts.router, prefix="/api/v1", tags=["prompts"])
 app.include_router(execute.router, prefix="/api/v1", tags=["execute"])
 
