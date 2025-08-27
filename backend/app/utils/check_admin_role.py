331364a82079226205e74d3a2c1eee249b08d656
Cristian Branet <cristix231@gmail.com>
2025-08-27 17:18:21 +0000

    Updated util

--- backend/app/utils/check_admin_role.py
+++ backend/app/utils/check_admin_role.py
@@ -1,8 +1,12 @@
 from fastapi import HTTPException, Depends
 from app.auth import auth
+from typing import Dict
 
-def check_admin_role(data: dict = Depends(auth.valid_access_token)):
-    roles = data.get("realm_access", {}).get("roles", [])
-    if "admin" not in roles:
-        raise HTTPException(status_code=403, detail="Forbidden")
-    return data
+async def check_admin_role(token_data: Dict = Depends(auth.valid_access_token)):
+    try:
+        roles = token_data.get("realm_access", {}).get("roles", [])
+        if "admin" not in roles:
+            raise HTTPException(status_code=403, detail="Access denied")
+        return token_data
+    except KeyError:
+        raise HTTPException(status_code=403, detail="Access denied")
